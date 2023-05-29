47670384fd0c44455a4ef4ca58405fd01d385d0e
Partial <133568898+Partial206@users.noreply.github.com>
2023-05-29 21:48:40 +0300

    Create pyautogui.py

---
+++ pyautogui.py
@@ -0,0 +1,13 @@
+**import pyautogui** : pyautogui yi import eder. Eğer farklı bir harfle kullanmak istiyorsan : import pyautogui as py
+
+**pyautogui.click(1)** : 1 Kez Mouse ile tıklar.
+**pyautogui.rightclick(1)** : 1 Kez sağ tıklar.
+**pyautogui.leftclick(1)** : 1 Kez sol tıklar.
+**pyautogui.drag(x, y)** : İstediğiniz X Ve Y konumuna Mousenizi götürür.
+**pyautogui.hotkey("alt","r")** : 1 tuşa basmak yerine 2 tane tuşa basabileceğiniz bir fonksiyon.
+**pyautogui.press("enter")**: istediğiniz bir tuşa basmak için.
+**pyautogui.locateCenterOnScreen("bla.png")** : bla.png i ekranda gördüğü gibi eğer bir değişkene eşitlerseniz kordinaatları verir eğer if e bağlarsanız görürse fonksiyonu yapar
+**pyautogui.position() : ** Mousenin o anki konumunu alabilirsiniz
+**pyautogui.typewrite("adana") : ** Yazı yazar klavye işlevi görür
+**pyautogui.alert(text="adana") : ** Hata kutusuna benzer kutu çıkartır
+**pyautogui.prompt(text="adana") : ** text kutusu çıkartır
